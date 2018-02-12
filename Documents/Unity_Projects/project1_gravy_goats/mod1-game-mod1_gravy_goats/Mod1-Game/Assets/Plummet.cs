02edf99f89d8a35da9723454a3d3c62c74ac6b9c
Alan Rivera <alan.rivera01@utrgv.edu>
2018-02-12 16:16:45 -0600

    added plummet script

---
+++ Documents/Unity_Projects/project1_gravy_goats/mod1-game-mod1_gravy_goats/Mod1-Game/Assets/Plummet.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plummet : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Collider is on trigger.");
+        Vector3 velocity = new Vector2(0, -20f);
+
+        transform.position = transform.position + (velocity * Time.deltaTime);
+
+    }
+}
